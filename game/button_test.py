98205234b92d390c798b8b2e9aeab9f88bfd3739
Szymon <szyszkaimyszka@gmail.com>
2025-01-31 13:46:07 +0100

    Commented button test and lights out layout

--- game/button_test.py
+++ game/button_test.py
@@ -5,15 +5,18 @@ window = tkinter.Tk()
 window.title("Lights out")
 window.geometry("600x400")
 
-
+# testing if button is aware of it's color
+# create button and add to window
 bobTheButton = GameButton(window)
 
+# print it's current color
 print(bobTheButton.get_color())
-
+# perform click
 bobTheButton.click()
-
+# print it's current color
 print(bobTheButton.get_color())
 
+# perform clicking and check color 3 times
 for i in range(3):
     bobTheButton.click()
     print(bobTheButton.get_color())
--- game/lights_out.py
+++ game/lights_out.py
@@ -1,19 +1,20 @@
 import tkinter as tkinter
 from tkinter import *
 from button import GameButton
-
+# create game window
 window = tkinter.Tk()
 window.title("Lights out")
 window.geometry("600x400")
 
+# create 1x2x1 grid
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight= 1)
 window.grid_columnconfigure(1, weight=2)
 window.grid_columnconfigure(2, weight= 1)
 
+# create game in middle cell
 gameBox = Frame(window)
 gameBox.grid(row=0,column=1,sticky="nesw")
-
 
 for i in range(0,5):
     gameBox.grid_rowconfigure(i, weight=1)
