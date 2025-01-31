b946173722dc8bb050fb1940403be771f525e209
Szymon <szyszkaimyszka@gmail.com>
2025-01-31 16:42:52 +0100

    gamespace early stage

---
+++ game/gameSpace.py
@@ -0,0 +1,8 @@
+import tkinter as tkinter
+from tkinter import *
+from button import GameButton
+
+class GameSpace(tkinter.Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
